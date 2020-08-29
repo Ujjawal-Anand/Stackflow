ac7238d2555860882a22a69c65654fbf33ab2b44
andy1729 <ujjawalanand1729@gmail.com>
2020-08-30 03:26:35 +0530

    fixed bugs

--- StackFlow/settings.py
+++ StackFlow/settings.py
@@ -169,4 +169,38 @@ WEBSITE_TAGLINE = 'Search Stackoverflow'
 HOMEPAGE_URL = '/'
 
 QUESTIONS_PER_PAGE=10
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+DEBUG_PROPAGATE_EXCEPTIONS = True
 django_heroku.settings(locals())
