9433de5db3a2022258b3ce020311e693d695efe0
andy1729 <ujjawalanand1729@gmail.com>
2020-08-29 20:38:29 +0000

    heroku setup

--- StackFlow/settings.py
+++ StackFlow/settings.py
@@ -94,11 +94,16 @@ WSGI_APPLICATION = 'StackFlow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(
+        default="postgres://stackflow:stackflow@localhost:5432/stackflow", conn_max_age=600
+    )
 }
 
 
