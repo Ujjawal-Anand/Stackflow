70da8fd9bb388ed01fb814779a9dfe5677289f5c
andy1729 <ujjawalanand1729@gmail.com>
2020-09-02 06:42:41 +0000

    added about and details pages

--- StackFlow/settings.py
+++ StackFlow/settings.py
@@ -94,18 +94,18 @@ WSGI_APPLICATION = 'StackFlow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default="postgres://stackflow:stackflow@localhost:5432/stackflow", conn_max_age=600
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default="postgres://stackflow:stackflow@localhost:5432/stackflow", conn_max_age=600
+    )
+}
 
 
 # Password validation
